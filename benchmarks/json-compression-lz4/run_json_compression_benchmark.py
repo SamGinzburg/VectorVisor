b0db13a66c39f2b81aa704d7b1ee3aae682d53c8
SamGinzburg <ginzburg.sam@gmail.com>
2021-03-23 22:08:31 -0400

    adding json-compression-lz4 benchmark

---
+++ benchmarks/json-compression-lz4/run_json_compression_benchmark.py
@@ -0,0 +1,104 @@
+# this is a helper script to invoke a batch of requests
+import requests
+import json
+from time import time
+from multiprocessing import Pool
+import sys
+import random
+import string
+
+def send_request_batch(req_list_ip_tuple):
+    req_list = req_list_ip_tuple[0]
+    port = req_list_ip_tuple[1]
+    num_batches_to_run = req_list_ip_tuple[2]
+    # do 100 batches
+    e2e_times = []
+    average_on_device_times = []
+    average_queue_times = []
+    for x in range(num_batches_to_run):
+        t0 = time()
+        r = requests.get('http://localhost:{}/batch_submit/'.format(port), json={"requests": req_list})
+        t1 = time()
+        on_device_times = []
+        device_queue_times = []
+        for resp in r.json()['requests']:
+            #print (r.json()['requests'][resp])
+            on_device_times.append(r.json()['requests'][resp]['on_device_execution_time_ns'])
+            device_queue_times.append(r.json()['requests'][resp]['device_queue_overhead_time_ns'])
+        e2e_times.append(t1-t0)
+        average_on_device_times.append(sum(on_device_times) / len(on_device_times))
+        average_queue_times.append(sum(device_queue_times) / len(device_queue_times))
+
+    #print (r.status_code)
+    #print (r.json())
+    #print ('Request took: %f seconds' %(t1-t0))
+    return (e2e_times, average_on_device_times, average_queue_times)
+
+if __name__ == '__main__':
+    # batch size
+
+    if len(sys.argv) >= 1:
+        try:
+            BATCH_SIZE=int(sys.argv[1])
+        except Exception as e:
+            BATCH_SIZE=64
+    else:
+        BATCH_SIZE=64
+
+    if len(sys.argv) >= 2:
+        PORT=int(sys.argv[2])
+    else:
+        PORT=8000
+
+    if len(sys.argv) >= 3:
+        NUM_VMM=int(sys.argv[3])
+    else:
+        NUM_VMM=1
+
+    if len(sys.argv) >= 4:
+        NUM_BATCHES_TO_RUN=int(sys.argv[4])
+    else:
+        NUM_BATCHES_TO_RUN=10
+
+    req_submit_list = []
+    # for each VMM we are targeting, create a batch
+    for vmm_idx in range(NUM_VMM):
+        # create a batch
+        req_list = []
+        for x in range(BATCH_SIZE):
+            # generate random string
+            random_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(4096*2,4096*4)))
+
+            # what we are sending the serverless function
+            payload = {
+                "text": random_str,
+            }
+
+            # some formatting stuff for the runtime
+            sample_req = {
+                "req_id": 0,
+                "req": json.dumps(payload),
+            }
+            req_list.append(sample_req)
+        req_submit_list.append((req_list, PORT + vmm_idx, NUM_BATCHES_TO_RUN))
+
+
+    # we can use this to ping multiple VMMs in parallel
+    p = Pool(5)
+    times = p.map(send_request_batch, req_submit_list)
+
+
+    print ("Batch size for each req: ", BATCH_SIZE)
+    print ("Total number of batches: ", NUM_BATCHES_TO_RUN)
+
+    for vmm, idx in zip(times, range(len(times))):
+        e2e_times = vmm[0]
+        on_device_time = vmm[1]
+        queue = vmm[2]
+
+        print ("VMM: ", idx)
+        print ("Total E2E (s): ", sum(e2e_times))
+        print ("Requests Per Second: ", (BATCH_SIZE * NUM_BATCHES_TO_RUN) / sum(e2e_times))
+        print ("Average E2E (s): ", sum(e2e_times) / len(e2e_times))
+        print ("Average on device time (ns): ", sum(on_device_time) / len(on_device_time))
+        print ("Average queue submit overhead (ns): ", sum(queue) / len(queue))
---
+++ benchmarks/json-compression-lz4/src/main.rs
@@ -0,0 +1,30 @@
+extern crate base64;
+
+use wasm_serverless_invoke::wasm_handler;
+use wasm_serverless_invoke::wasm_handler::WasmHandler;
+use serde_json::Value;
+use serde_json::json;
+
+
+use base64::encode;
+use lz4_flex::{compress};
+
+fn compress_json(event: Value) -> Value {
+    let response = match event.get("text") {
+        Some(Value::String(str)) => {
+            let compressed_str = compress(&str.as_bytes());
+            json!(encode(compressed_str))
+        },
+        _ => {
+            // input is not a string we can compress!, no-op
+            json!(null)
+        }
+    };
+
+    response
+}
+
+fn main() {
+    let handler = WasmHandler::new(&compress_json);
+    handler.run();
+}
