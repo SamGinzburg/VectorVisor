69daa3d360be1a71205403862d21bd9772c172ae
EC2 Default User <ec2-user@ip-172-31-42-203.ec2.internal>
2021-05-26 04:04:52 +0000

    add helper bash script

---
+++ benchmarks/start_lz4_server.sh
@@ -0,0 +1,4 @@
+for i in {0..15}
+do
+	cargo run --release -- -i json-compression.wasm.partbin -h 3145728 --partition=true --vmcount 64 --serverless=true --port=$((8000 + $i)) --ip 0.0.0.0 --vmgroups=1 &>/dev/null &
+done
