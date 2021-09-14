7b304fd8ea88cc27af5edf0b4d000cefbd37e0ce
Sam Ginzburg <ginzburg.sam@gmail.com>
2021-09-13 23:30:57 -0400

    minor fix

--- src/opencl_runner/vectorized_vm.rs
+++ src/opencl_runner/vectorized_vm.rs
@@ -160,7 +160,7 @@ impl VectorizedVM {
 
         let engine = Engine::default();
         let store = Store::new(&engine);
-        let memory_ty = MemoryType::new(Limits::new(hcall_buf_size / (1024 * 64), None));
+        let memory_ty = MemoryType::new(Limits::new(1, None));
         let memory = Memory::new(&store, memory_ty);
 
         VectorizedVM {
