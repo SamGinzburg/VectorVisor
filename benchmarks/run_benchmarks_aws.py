43df256b4120dc7602d19edcf6a0791edc11a4eb
Sam Ginzburg <ginzburg.sam@gmail.com>
2021-09-28 19:41:55 -0400

    update testing script

--- benchmarks/run_benchmarks_aws.py
+++ benchmarks/run_benchmarks_aws.py
@@ -57,6 +57,9 @@ userdata = """#cloud-config
      - cd ..
      - cd imageblur/
      - ~/.cargo/bin/cargo build --release
+     - cd ..
+     - cd imagehash/
+     - ~/.cargo/bin/cargo build --release
 """ % region
 
 
@@ -109,6 +112,10 @@ userdata_ubuntu = """#cloud-config
      - cd imageblur/
      - sudo ~/.cargo/bin/cargo build --release
      - wasm-opt target/wasm32-wasi/release/imageblur.wasm {opt} -c -o target/wasm32-wasi/release/imageblur-opt.wasm
+     - cd ..
+     - cd imagehash/
+     - sudo ~/.cargo/bin/cargo build --release
+     - wasm-opt target/wasm32-wasi/release/imagehash.wasm {opt} -c -o target/wasm32-wasi/release/imagehash-opt.wasm
 """.format(opt=OPT_LEVEL)
 
 
@@ -460,8 +467,110 @@ def run_average_bench():
     with open("cpu_bench_average.txt", "w") as text_file:
         text_file.write(str(output))
 
+def run_image_hash_bench():
+    run_image_command_wasmtime = """#!/bin/bash
+    sudo su
+    ulimit -n 65536
+    x=$(cloud-init status)
+    until [ "$x" == "status: done" ]; do
+    sleep 10
+    x=$(cloud-init status)
+    done
 
-def run_image_bench():
+    /tmp/wasm2opencl/target/release/wasm2opencl --input /tmp/wasm2opencl/benchmarks/imagehash/target/wasm32-wasi/release/imagehash-opt.wasm --ip=0.0.0.0 --heap=4194304 --stack=262144 --hcallsize=524288 --partition=true --serverless=true --vmcount=3072 --wasmtime=true --fastreply={fastreply} &> /tmp/imagehash.log &
+    """.format(fastreply=fastreply)
+
+    run_command(run_image_command_wasmtime, "run_imagehash_command_wasmtime", cpu_bench_instance[0].id)
+
+    run_image_command = """#!/bin/bash
+    sudo su
+    ulimit -n 65536
+    x=$(cloud-init status)
+    until [ "$x" == "status: done" ]; do
+    sleep 10
+    x=$(cloud-init status)
+    done
+
+    /tmp/wasm2opencl/target/release/wasm2opencl --input /tmp/wasm2opencl/benchmarks/imagehash/target/wasm32-wasi/release/imagehash-opt.wasm --ip=0.0.0.0 --heap=4194304 --stack=262144 --hcallsize=524288 --partition=true --serverless=true --vmcount=3072 --vmgroups=1 --maxdup=3 --disablefastcalls=false --partitions=50 --maxloc=2000000 --lgroup={lgroup} --cflags={cflags} --interleave={interleave} --pinput={is_pretty} --fastreply={fastreply} &> /tmp/imagehash.log &
+    """.format(lgroup=local_group_size, cflags=CFLAGS, interleave=interleave, is_pretty=is_pretty, fastreply=fastreply)
+
+    run_command(run_image_command, "run_imagehash_gpu_command", gpu_instance[0].id)
+
+    # Now set up the invoker
+
+    run_invoker = """#!/bin/bash
+    sudo su
+    ulimit -n 65536
+    mkdir -p ~/gocache/
+    mkdir -p ~/gopath/
+    mkdir -p ~/xdg/
+    export GOCACHE=~/gocache/
+    export GOPATH=~/gopath/
+    export XDG_CACHE_HOME=~/xdg/
+
+    x=$(cloud-init status)
+    until [ "$x" == "status: done" ]; do
+    sleep 10
+    x=$(cloud-init status)
+    done
+
+    cd /tmp/wasm2opencl/benchmarks/imagehash/
+
+    /usr/local/go/bin/go run run_image_hash.go {addr} 8000 {target_rps} 1 120
+
+    /usr/local/go/bin/go run run_image_hash.go {addr} 8000 {target_rps} 1 120
+    """.format(addr=gpu_instance[0].private_dns_name, input_size=1000, target_rps=target_rps)
+
+
+    command_id = run_command(run_invoker, "run invoker for gpu", invoker_instance[0].id)
+
+    time.sleep(20)
+
+    # Block until benchmark is complete
+    output = block_on_command(command_id, invoker_instance[0].id)
+    print (output)
+
+    # save output
+    with open("gpu_bench_imagehash.txt", "w") as text_file:
+        text_file.write(str(output))
+
+    run_invoker_wasmtime = """#!/bin/bash
+    sudo su
+    ulimit -n 65536
+    mkdir -p ~/gocache/
+    mkdir -p ~/gopath/
+    mkdir -p ~/xdg/
+    export GOCACHE=~/gocache/
+    export GOPATH=~/gopath/
+    export XDG_CACHE_HOME=~/xdg/
+
+    x=$(cloud-init status)
+    until [ "$x" == "status: done" ]; do
+    sleep 10
+    x=$(cloud-init status)
+    done
+
+    cd /tmp/wasm2opencl/benchmarks/imagehash/
+
+    /usr/local/go/bin/go run run_image_hash.go {addr} 8000 {target_rps} 1 120
+
+    /usr/local/go/bin/go run run_image_hash.go {addr} 8000 {target_rps} 1 120
+    """.format(addr=cpu_bench_instance[0].private_dns_name, input_size=1000, target_rps=target_rps_cpu)
+
+    command_id = run_command(run_invoker_wasmtime, "run invoker for cpu", invoker_instance[0].id)
+
+    time.sleep(20)
+
+    # Block until benchmark is complete
+    output = block_on_command(command_id, invoker_instance[0].id)
+    print (output)
+    # save output
+    with open("cpu_bench_imagehash.txt", "w") as text_file:
+        text_file.write(str(output))
+
+
+
+def run_image_blur_bench():
     run_image_command_wasmtime = """#!/bin/bash
     sudo su
     ulimit -n 65536
@@ -691,7 +800,7 @@ p3.2xlarge   => 1 V100, 16 GiB memory, 8 vCPU, $3.06 / hr
 # ami-00339339e800db52e  ==> OpenCL 1.2 driver (460.X)
 # ami-0748c95fd9dd9f42a  ==> OpenCL 1.2 driver (450.X)
 gpu_instance = ec2.create_instances(ImageId='ami-01463836f7041cd10',
-                                InstanceType="p3.2xlarge",
+                                InstanceType="g4dn.2xlarge",
                                 MinCount=1,
                                 MaxCount=1,
                                 UserData=userdata_ubuntu,
@@ -781,9 +890,13 @@ ssm_client = boto3.client('ssm')
 
 #cleanup()
 
-# run image bench
-run_image_bench()
+# run image blue bench
+#run_image_blur_bench()
 
+#cleanup()
+
+# run image hash bench
+run_image_hash_bench()
 
 # clean up all instances at end
 ec2.instances.filter(InstanceIds = instance_id_list).terminate()
