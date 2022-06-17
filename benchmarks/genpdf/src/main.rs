bf29274e0eb9e50624dd4758669cf94626611c5e
SamGinzburg <ginzburg.sam@gmail.com>
2022-06-16 22:57:01 -0400

    try other genpdf lib

--- benchmarks/genpdf/src/main.rs
+++ benchmarks/genpdf/src/main.rs
@@ -1,10 +1,52 @@
-use printpdf::*;
-use std::fs::File;
-use std::io::BufWriter;
+use lopdf::dictionary;
+use lopdf::{Document, Object, Stream};
+use lopdf::content::{Content, Operation};
 
 fn main() {
-    let (doc, page1, layer1) = PdfDocument::new("PDF_Document_title", Mm(247.0), Mm(210.0), "Layer 1");
-    let (page2, layer1) = doc.add_page(Mm(10.0), Mm(250.0),"Page 2, Layer 1");
-    let bytes = doc.save_to_bytes().unwrap();
-    println!("bytes: {:?}", bytes);
+    let mut result: Vec<u8> = vec![];
+    let mut doc = Document::with_version("1.5");
+    let pages_id = doc.new_object_id();
+    let font_id = doc.add_object(dictionary! {
+        "Type" => "Font",
+        "Subtype" => "Type1",
+        "BaseFont" => "Courier",
+    });
+    let resources_id = doc.add_object(dictionary! {
+        "Font" => dictionary! {
+            "F1" => font_id,
+        },
+    });
+    let content = Content {
+        operations: vec![
+            Operation::new("BT", vec![]),
+            Operation::new("Tf", vec!["F1".into(), 48.into()]),
+            Operation::new("Td", vec![100.into(), 600.into()]),
+            Operation::new("Tj", vec![Object::string_literal("Hello World!")]),
+            Operation::new("ET", vec![]),
+        ],
+    };
+    let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+    let page_id = doc.add_object(dictionary! {
+        "Type" => "Page",
+        "Parent" => pages_id,
+        "Contents" => content_id,
+    });
+    let pages = dictionary! {
+        "Type" => "Pages",
+        "Kids" => vec![page_id.into()],
+        "Count" => 1,
+        "Resources" => resources_id,
+        "MediaBox" => vec![0.into(), 0.into(), 595.into(), 842.into()],
+    };
+    doc.objects.insert(pages_id, Object::Dictionary(pages));
+    let catalog_id = doc.add_object(dictionary! {
+        "Type" => "Catalog",
+        "Pages" => pages_id,
+    });
+    doc.trailer.set("Root", catalog_id);
+    doc.compress();
+    doc.save_to(&mut result).unwrap();
+    //doc.save("test.pdf").unwrap();
+
+    println!("{:?}", result);
 }
