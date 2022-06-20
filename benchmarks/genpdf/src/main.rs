526da5d93aa9ef028cc150515af51797f727a876
SamGinzburg <ginzburg.sam@gmail.com>
2022-06-20 18:59:29 -0400

    updates to genpdf to reduce code bloat

--- benchmarks/genpdf/src/main.rs
+++ benchmarks/genpdf/src/main.rs
@@ -11,6 +11,7 @@ use wasm_serverless_invoke::wasm_handler::SerializationFormat::MsgPack;
 use serde::Deserialize;
 use serde::Serialize;
 use lopdf::Dictionary;
+use lopdf::Object::Name;
 
 lazy_static! {
     static ref EMBED_IMAGE: &'static [u8] = include_bytes!("test.jpg");
@@ -26,27 +27,6 @@ struct FuncInput {
 #[derive(Debug, Serialize)]
 struct FuncResponse {
     resp: Vec<u8>
-}
-
-#[inline(never)]
-fn insert_img(doc: &mut Document, page_id: (u32, u16)) {
-    let mut dict = Dictionary::new();
-    dict.set("Type", Object::Name(b"XObject".to_vec()));
-    dict.set("Subtype", Object::Name(b"Image".to_vec()));
-    dict.set("Width", 814);
-    dict.set("Height", 613);
-    dict.set("ColorSpace", Object::Name(b"DeviceRGB".to_vec()));
-    dict.set("BitsPerComponent", 8);
-    dict.set("Filter", Object::Name(b"DCTDecode".to_vec()));
-
-    //let img = image::load_from_memory(EMBED_IMAGE.to_vec().as_ref()).unwrap();
-    //let bits = img.color().bits_per_pixel() / 3;
-    //dbg!(bits);
-
-    let img_stream = Stream::new(dict, EMBED_IMAGE.to_vec());
-    //let img_stream = xobject::image_from(EMBED_IMAGE.to_vec()).unwrap();
-    doc.insert_image(page_id, img_stream, (100.0, 210.0), (100.0+(814.0/3.0), 210.0+(613.0/3.0))).unwrap();
-
 }
 
 #[inline(never)]
@@ -97,6 +77,44 @@ fn genpdf(event: FuncInput) -> FuncResponse {
     }
     pdf_ops.extend(purchase_ops);
 
+    let mut image_ops: Vec<Operation> = vec![];
+
+    // Add an image
+    // The default "insert_image" API results in massive code bloat, adding extra compression routines for no reason
+    let mut dict = Dictionary::new();
+    dict.set("Type", Object::Name(b"XObject".to_vec()));
+    dict.set("Subtype", Object::Name(b"Image".to_vec()));
+    dict.set("Width", 814);
+    dict.set("Height", 613);
+    dict.set("ColorSpace", Object::Name(b"DeviceRGB".to_vec()));
+    dict.set("BitsPerComponent", 8);
+    // For JPG files
+    dict.set("Filter", Object::Name(b"DCTDecode".to_vec()));
+
+    let img_stream = Stream::new(dict, EMBED_IMAGE.to_vec());
+
+    let img_position = (100.0, 210.0);
+    let img_size = (100.0+(814.0/3.0), 210.0+(613.0/3.0));
+    let img_id = doc.add_object(img_stream);
+    let img_name = format!("X{}", img_id.0);
+    image_ops.push(Operation::new("q", vec![]));
+    image_ops.push(Operation::new(
+        "cm",
+        vec![
+            img_size.0.into(),
+            0.into(),
+            0.into(),
+            img_size.1.into(),
+            img_position.0.into(),
+            img_position.1.into(),
+        ],
+    ));
+    image_ops.push(Operation::new("Do", vec![Name(img_name.as_bytes().to_vec())]));
+    image_ops.push(Operation::new("Q", vec![]));
+    image_ops.push(Operation::new("Q", vec![]));
+
+    pdf_ops.extend(image_ops);
+
     let content = Content {
         operations: pdf_ops,
     };
@@ -108,26 +126,7 @@ fn genpdf(event: FuncInput) -> FuncResponse {
         "Contents" => content_id,
     });
 
-    /*
-    let mut dict = Dictionary::new();
-    dict.set("Type", Object::Name(b"XObject".to_vec()));
-    dict.set("Subtype", Object::Name(b"Image".to_vec()));
-    dict.set("Width", 814);
-    dict.set("Height", 613);
-    dict.set("ColorSpace", Object::Name(b"DeviceRGB".to_vec()));
-    dict.set("BitsPerComponent", 8);
-    //dict.set("Filter", Object::Name(b"DCTDecode".to_vec()));
-
-    //let img = image::load_from_memory(EMBED_IMAGE.to_vec().as_ref()).unwrap();
-    //let bits = img.color().bits_per_pixel() / 3;
-    //dbg!(bits);
-
-    let img_stream = Stream::new(dict, EMBED_IMAGE.to_vec());
-    //let img_stream = xobject::image_from(EMBED_IMAGE.to_vec()).unwrap();
-    doc.insert_image(page_id, img_stream, (100.0, 210.0), (100.0+(814.0/3.0), 210.0+(613.0/3.0))).unwrap();
-    */
-
-    insert_img(&mut doc, page_id);
+    doc.add_xobject(page_id, img_name.as_bytes(), img_id).unwrap();
 
     let pages = dictionary! {
         "Type" => "Pages",
@@ -144,7 +143,7 @@ fn genpdf(event: FuncInput) -> FuncResponse {
     doc.trailer.set("Root", catalog_id);
     //doc.compress();
     doc.save_to(&mut result).unwrap();
-    //doc.save("test.pdf").unwrap();
+    doc.save("test.pdf").unwrap();
     //println!("{:?}", result);
 
     FuncResponse{ resp: result }
