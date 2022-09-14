ac04de8e9db86a1e6474d3b2009cf70adb88e2a1
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-09-13 21:53:40 -0400

    debug attempt to see if this gets triggered

--- src/opencl_runner/serverless.rs
+++ src/opencl_runner/serverless.rs
@@ -29,6 +29,7 @@ impl Serverless {
         // If other non-invoke calls need to be dispatched, perform a no-op and return.
         // This call will be executed later when ready
         if hypercall.non_serverless_invoke_call_found {
+            dbg!("non_serverless_invoke_call_found!!!");
             return;
         }
         
