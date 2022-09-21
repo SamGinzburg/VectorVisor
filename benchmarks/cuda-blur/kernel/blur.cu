d81d384de170d9d010d19d1d524540122ff67a4f
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-09-21 14:46:40 -0400

    add cuda-blur native bench

---
+++ benchmarks/cuda-blur/kernel/blur.cu
@@ -0,0 +1,21 @@
+extern "C" __global__ void blur(const unsigned char *input, unsigned char *output, const unsigned int width, const unsigned int height, const float *kernel, const unsigned int kernelWidth) {
+
+    const unsigned int col = threadIdx.x + blockIdx.x * blockDim.x;
+    const unsigned int row = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if(row < height && col < width) {
+        const int half = kernelWidth / 2;
+        float blur = 0.0;
+        for(int i = -half; i <= half; i++) {
+            for(int j = -half; j <= half; j++) {
+
+                const unsigned int y = max(0, min(height - 1, row + i));
+                const unsigned int x = max(0, min(width - 1, col + j));
+
+                const float w = kernel[(j + half) + (i + half) * kernelWidth];
+                blur += w * input[x + y * width];
+            }
+        }
+        output[col + row * width] = static_cast<unsigned char>(blur);
+    }
+}
