396d49e80f009b913f6ba16deccf7c1b3e7a6aeb
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-10-26 14:16:41 -0400

    changes to save cache bin for testing

--- benchmarks/save_cached_bin.sh
+++ benchmarks/save_cached_bin.sh
@@ -19,15 +19,19 @@ function cachebin() {
   cp ${1}/target/wasm32-wasi/release/${1}.wasm .
   wasm-snip ${1}.wasm --snip-rust-panicking-code -o ${1}-snip.wasm -p rust_oom __rg_oom
   wasm-opt ${1}-snip.wasm -O1 -g -c -o ${1}-opt.wasm
+  cp ${1}-opt.wasm ${1}-opt-4.wasm
+  cp ${1}-opt.wasm ${1}-opt-8.wasm
   cp ${1}-opt.wasm a10g_${1}-opt.wasm
-  cargo run --release -- -i $1-opt.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$5 --partition=false --maxdup=0 --jt=true --interleave=4
-  #cargo run --release -- -i a10g_$1-opt.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$6 --partition=false --maxdup=0 --jt=true --interleave=4
+  cp ${1}-opt.wasm a10g_${1}-opt-4.wasm
+  cp ${1}-opt.wasm a10g_${1}-opt-8.wasm
+  cargo run --release -- -i $1-opt-4.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$5 --partition=false --maxdup=0 --jt=true --interleave=4
+  #cargo run --release -- -i $1-opt-8.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$5 --partition=false --maxdup=0 --jt=true --interleave=8
+  #cargo run --release -- -i a10g_$1-opt-4.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$6 --partition=false --maxdup=0 --jt=true --interleave=4
+  #cargo run --release -- -i a10g_$1-opt-8.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$6 --partition=false --maxdup=0 --jt=true --interleave=8
 }
 
-cachebin "imageblur-bmp" "4194304" "262144" "409600" "2048" "2048"
-#cachebin "genpdf" "3145728" "131072" "262144" "2048" "2048"
-exit
 cachebin "pbkdf2" "3145728" "262144" "131072" "4096" "6144"
+exit
 cachebin "imagehash" "4194304" "131072" "262144" "3072" "4608"
 cachebin "imagehash-modified" "4194304" "131072" "262144" "3072" "4608"
 cachebin "imageblur" "4194304" "262144" "409600" "3072" "4608"
@@ -36,13 +40,13 @@ cachebin "json-compression" "4194304" "131072" "524288" "3072" "4608"
 cachebin "scrypt" "3145728" "262144" "131072" "4096" "6144"
 cachebin "average" "3145728" "131072" "262144" "4096" "5120"
 cachebin "nlp-count-vectorizer" "4194304" "131072" "524288" "3072" "4608"
-cachebin "genpdf" "3145728" "131072" "262144" "4096" "5120"
+#cachebin "genpdf" "3145728" "131072" "262144" "4096" "5120"
 
 # Save the generated *.bin files
-tar -zcvf nvbin.backup $( find -name "*.bin" )
+#tar -zcvf nvbin.backup $( find -name "*.bin" )
 
 # save the nvcache
-tar -zcvf nvcache.backup -C ~/.nv/ComputeCache/ .
+#tar -zcvf nvcache.backup -C ~/.nv/ComputeCache/ .
 
 # restore nvcache
 # tar -zxvf nvcache.backup -C ~/.nv/ComputeCache/ .
