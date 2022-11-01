24c2c0b3a92187bd0b7107e7c46f053ad9a1cc66
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-11-01 14:52:07 -0400

    update file for cachebin

--- benchmarks/save_cached_bin.sh
+++ benchmarks/save_cached_bin.sh
@@ -30,6 +30,7 @@ function cachebin() {
   #cargo run --release -- -i a10g_$1-opt-8.wasm --heap=$2 --stack=$3 --hcallsize=$4 --vmcount=$6 --partition=false --maxdup=0 --jt=true --interleave=8
 }
 
+cachebin "rust-pdfwriter" "4194304" "131072" "409600" "3072" "4608"
 cachebin "pbkdf2" "3145728" "262144" "131072" "4096" "6144"
 cachebin "imagehash" "4194304" "131072" "262144" "3072" "4608"
 cachebin "imagehash-modified" "4194304" "131072" "262144" "3072" "4608"
