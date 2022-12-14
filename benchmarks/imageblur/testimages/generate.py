ad93529ad5d5a5034b967e7221c15cfb13c76c6b
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-12-13 21:17:07 -0500

    image sizes for blur/hash benches should be the same for jpg/bmp

--- benchmarks/imageblur/testimages/generate.py
+++ benchmarks/imageblur/testimages/generate.py
@@ -2,7 +2,7 @@ import numpy
 from PIL import Image
 
 for x in range(64):
-    imarray = numpy.random.rand(400,400,3) * 255
+    imarray = numpy.random.rand(200,200,3) * 255
     im = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
     rgb_im = im.convert('RGB')
     rgb_im.save('{idx}.jpg'.format(idx=x))
--- benchmarks/imagehash/testimages/generate.py
+++ benchmarks/imagehash/testimages/generate.py
@@ -2,7 +2,7 @@ import numpy
 from PIL import Image
 
 for x in range(64):
-    imarray = numpy.random.rand(350,350,3) * 255
+    imarray = numpy.random.rand(256,256,3) * 255
     im = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
     rgb_im = im.convert('RGB')
     rgb_im.save('{idx}.jpg'.format(idx=x))
